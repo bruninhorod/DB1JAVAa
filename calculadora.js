611b4adcae8c428e5a981a89eeb942f89f23e4e7
bruninhorod <55368077+bruninhorod@users.noreply.github.com>
2020-02-13 22:18:05 -0300

    Primeira aula de Web - Não finalizado.

---
+++ calculadora.js
@@ -0,0 +1,46 @@
+var num1,num2;
+var somaa, divisao,multiplicacao,subtracao;
+var calc;
+
+
+function calculadora(){
+   
+}
+
+function soma(){
+    console.log("Olá akak")
+    //return somaa = num1 + num2;
+}
+
+function subtrair(num1,num2){
+   return subtracao = num1 - num2; 
+}
+
+function dividir(num1,num2){
+   return divisao = num1/num2;
+}
+
+function multiplicar(num1,num2){
+   return multiplicacao = num1*num2;
+}
+
+switch (calc){
+
+case 1:
+    soma;
+break;
+
+case 2:
+    subtrair;
+break;
+
+case 3:
+    divisao;
+break;
+
+case 4:
+    multiplicacao;
+break;
+
+
+}
